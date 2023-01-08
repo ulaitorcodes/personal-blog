c10f7543bd4ea80ee1a24582eff16a51935c4eb3
buffetng <ulaitorcodes@gmail.com>
2023-01-08 02:16:03 +0100

    with events

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,19 @@
 const Home = () => {
 
+    const handClick = (e) =>{
+        console.log('Hello, Ninja', e)
+    }
+    const handleClickAgain = (name)=>{
+        console.log('Hello ', name)
+    }
+
     return ( 
         <div className="home">
             <h2>Home page</h2>
+            <button onClick={handClick}>Click</button>
+            <button onClick={() => handleClickAgain('Maria')}>Clickn Again</button>
+
+
         </div>
      );
 }
