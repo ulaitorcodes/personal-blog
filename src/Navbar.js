4490da0a17f5420869bc2e43218ed212acdad802
buffetng <ulaitorcodes@gmail.com>
2023-01-24 14:22:33 +0100

    router links

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,11 @@
+import { Link } from "react-router-dom";
 const Navbar = () => {
     return ( 
         <nav className="navbar">
             <h1>My personal Blog</h1>
             <div className="links">
-                <a href="/">home</a>
-                <a href="/create">New blog</a>
+                <Link to="/">home</Link>
+                <Link to="/create">New blog</Link>
             </div>
         </nav>
     );
