6c9801adae4e68429c8c7150c76564d38fe19cfb
buffetng <ulaitorcodes@gmail.com>
2023-01-25 14:38:52 +0100

    reusable custom hooks

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const BlogList = ({blogs, title}) => {
    
     return (
@@ -7,11 +9,11 @@ const BlogList = ({blogs, title}) => {
 
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
+                    <Link to={`/blogs/${blog.id}`}>
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
+                    </Link>
                      
-                    
-        
                 </div>
             ))}
             
