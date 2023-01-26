cdc5fb435751ea692d3a4795c097412b1a01e997
buffetng <ulaitorcodes@gmail.com>
2023-01-26 01:30:56 +0100

    submit event

--- src/Create.js
+++ src/Create.js
@@ -4,12 +4,17 @@ const Create = () =>{
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('el');
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        const blog = {title, body, author};
+        console.log(blog)
+    }
 
 
     return (
         <div className="create">
             <h2>Add a New Blog</h2>
-            <form action="">
+            <form onSubmit={handleSubmit}>
                 <label>Blog title</label>
                 <input type="text" required value={title} onChange={(e) => setTitle(e.target.value)}/>
 
